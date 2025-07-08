b95156f9a387b31508aaa6cec282fb3a6f352e0a
Suprotik007 <csuprotik@gmail.com>
2025-07-08 13:42:50 +0600

    first commit

---
+++ router.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router";
+import App from "./src/App";
+import RootLayout from "./src/Layout/RootLayout";
+
+const router = createBrowserRouter([
+
+    {
+        path: '/',
+        Component:RootLayout
+    }
+
+])
+
+export default router
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+   <div className='mt-5 text-yellow-500'>hello</div>
+    </>
+  )
+}
+
+export default App
